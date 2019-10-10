0d9f8b91b92d5279ef7a48795a617037705696a4
Tony <serviceantapl@yahoo.com>
2019-10-10 20:49:24 +0300

    3rd lesson

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-let money = +prompt ("Ваш бюджет на месяц?", ''),
+
+let money , time ;
+
+
+function start() {
+    money = +prompt ("Ваш бюджет на месяц?", '');
+    
+
+    while (isNaN(money) || money == '' || money == null ){
+        money = +prompt ("Ваш бюджет на месяц?", '');
+    }
+
     time = prompt ("Введите дату в формате YYYY-MM-DD", '');
+}
+
+start();
+
+
 
 let appData ={ 
     budget: money,
@@ -7,22 +23,69 @@ let appData ={
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
 //  ---- Это цикл с "for" ------
 
-for (let i = 0; i<2; i++) {
-    let a = prompt ("Статья расходов?", ''),
-        b = prompt ("Во сколько встанет?", '');
-    if ( typeof(a)=== "string" && typeof(a) !=null && typeof(b) !=null && a !='' && b !='' && a.length <10) {
-        console.log('done!');
-        appData.expenses[a] = b;
-    } else {
-        console.log('Вы где-то ввели недопустимое значение! Попробуйте снова!');
-        i--;
+function chooseExpenses () {
+    for (let i = 0; i<2; i++) {
+        let a = prompt ("Статья расходов?", ''),
+            b = prompt ("Во сколько встанет?", '');
+        if ( typeof(a)=== "string" && typeof(a) !=null && typeof(b) !=null && a !='' && b !='' && a.length <10) {
+            console.log('done!');
+            appData.expenses[a] = b;
+        } else {
+            console.log('Вы где-то ввели недопустимое значение! Попробуйте снова!');
+            i--;
+        }
     }
+    
 }
+
+chooseExpenses ();
+
+// appData.moneyPerDay = (appData.budget / 30).toFixed(3);
+
+// alert('Vash Budjet: ' + appData.moneyPerDay);
+
+//     if(appData.moneyPerDay < 100){
+//         alert('Маловато бабок у тебя сынок... надо чето думать');
+//     } else if(appData.moneyPerDay  > 2000){
+//         alert('Да ты жирный кот... где столько поднимаешь?');
+//     } else if(appData.moneyPerDay >=100 && appData.moneyPerDay<= 2000){
+//         alert('Ну не бедствуешь и ладно... как сам то братишка?');
+//     } else {
+//         alert('Гдето косяк братишка');
+//     }
+
+function dectectDayBudget(){
+
+    appData.moneyPerDay = (appData.budget / 30).toFixed(3);
+
+    alert('Vash Budjet: ' + appData.moneyPerDay);
+}
+
+dectectDayBudget();
+
+function detectLevel() {
+    if(appData.moneyPerDay < 100){
+        alert('Маловато бабок у тебя сынок... надо чето думать');
+    } else if(appData.moneyPerDay  > 2000){
+        alert('Да ты жирный кот... где столько поднимаешь?');
+    } else if(appData.moneyPerDay >=100 && appData.moneyPerDay<= 2000){
+        alert('Ну не бедствуешь и ладно... как сам то братишка?');
+    } else {
+        alert('Гдето косяк братишка');
+    }
+
+}
+
+detectLevel ();
+
+
+
+
 
 // ----- Это цикл с "while" -----
 // let i = 0
@@ -60,22 +123,33 @@ for (let i = 0; i<2; i++) {
 //     while (i < 2);
     
 
-appData.moneyPerDay = appData.budget / 30;
 
-alert('Vash Budjet: ' + appData.moneyPerDay);
 
-    if(appData.moneyPerDay < 100){
-        alert('Маловато бабок у тебя сынок... надо чето думать');
-    } else if(appData.moneyPerDay  > 2000){
-        alert('Да ты жирный кот... где столько поднимаешь?');
-    } else if(appData.moneyPerDay >=100 && appData.moneyPerDay< 2000){
-        alert('Ну не бедствуешь и ладно... как сам то братишка?');
-    } else {
-        alert('Гдето косяк братишка');
+function checkSavings (){
+    if (appData.savings == true) {
+        let save = +prompt('Какова сумма накоплений???'),
+            percent = +prompt('Под какой процент???');
+            
+        appData.monthIncome = save/100/12*percent;
+        alert ('Доход v mesyac с вашего депозита: ' + appData.monthIncome);
     }
 
+}
+
+checkSavings ();
 
 
+function chooseOptExpenses () {
+
+    for (let i =0; i<=2; i++) {
+
+        let questionOptExp = prompt('Статья необязательных расходов?');
+        appData.optionalExpenses[i] = questionOptExp;
+
+    }
+
+}
+chooseOptExpenses ();
 
 
 
