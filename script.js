91969b74ee2afda183a9ca317e8b386ef6a3df04
Tony <serviceantapl@yahoo.com>
2019-10-16 23:32:11 +0300

    lesson4

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 let money , time ;
 
 
@@ -15,157 +16,74 @@ function start() {
 
 start();
 
-
-
-let appData ={ 
+let appData = { 
     budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: true
-};
-
-//  ---- Это цикл с "for" ------
-
-function chooseExpenses () {
-    for (let i = 0; i<2; i++) {
-        let a = prompt ("Статья расходов?", ''),
-            b = prompt ("Во сколько встанет?", '');
-        if ( typeof(a)=== "string" && typeof(a) !=null && typeof(b) !=null && a !='' && b !='' && a.length <10) {
-            console.log('done!');
-            appData.expenses[a] = b;
-        } else {
-            console.log('Вы где-то ввели недопустимое значение! Попробуйте снова!');
-            i--;
-        }
-    }
-    
-}
-
-chooseExpenses ();
-
-// appData.moneyPerDay = (appData.budget / 30).toFixed(3);
-
-// alert('Vash Budjet: ' + appData.moneyPerDay);
-
-//     if(appData.moneyPerDay < 100){
-//         alert('Маловато бабок у тебя сынок... надо чето думать');
-//     } else if(appData.moneyPerDay  > 2000){
-//         alert('Да ты жирный кот... где столько поднимаешь?');
-//     } else if(appData.moneyPerDay >=100 && appData.moneyPerDay<= 2000){
-//         alert('Ну не бедствуешь и ладно... как сам то братишка?');
-//     } else {
-//         alert('Гдето косяк братишка');
-//     }
-
-function dectectDayBudget(){
-
-    appData.moneyPerDay = (appData.budget / 30).toFixed(3);
-
-    alert('Vash Budjet: ' + appData.moneyPerDay);
-}
-
-dectectDayBudget();
-
-function detectLevel() {
-    if(appData.moneyPerDay < 100){
-        alert('Маловато бабок у тебя сынок... надо чето думать');
-    } else if(appData.moneyPerDay  > 2000){
-        alert('Да ты жирный кот... где столько поднимаешь?');
-    } else if(appData.moneyPerDay >=100 && appData.moneyPerDay<= 2000){
-        alert('Ну не бедствуешь и ладно... как сам то братишка?');
-    } else {
-        alert('Гдето косяк братишка');
-    }
-
-}
-
-detectLevel ();
-
-
-
-
-
-// ----- Это цикл с "while" -----
-// let i = 0
-//     while (i < 2) {
-//         let a = prompt ("Статья расходов?", ''),
-//             b = prompt ("Во сколько встанет?", '');
-//         if ( typeof(a)=== "string" && typeof(a) !=null && typeof(b) !=null && a !='' && b !='' && a.length <10) {
-//             console.log('done!');
-//             appData.expenses[a] = b;
-//         } else {
-//             console.log('Вы где-то ввели недопустимое значение! Попробуйте снова!');
-//             i--;
-//         }
-
-//         i++;
-//     }
-
-
-// ---- Это цикл с "do" + "while" ----
-
-// let i = 0;
-// do {
-//     let a = prompt ("Статья расходов?", ''),
-//         b = prompt ("Во сколько встанет?", '');
-//     if ( typeof(a)=== "string" && typeof(a) !=null && typeof(b) !=null && a !='' && b !='' && a.length <10) {
-//         console.log('done!');
-//         appData.expenses[a] = b;
-//     } else {
-//         console.log('Вы где-то ввели недопустимое значение! Попробуйте снова!');
-//         i--;
-//     }
-
-//     i++;
-// }
-//     while (i < 2);
-    
-
-
-
-function checkSavings (){
-    if (appData.savings == true) {
-        let save = +prompt('Какова сумма накоплений???'),
-            percent = +prompt('Под какой процент???');
+    savings: true,
+    chooseExpenses: function() {
+        for (let i=0; i<2; i++) {
+            let a = prompt ('Введите обязательную строку расходов в этом месяце',''),
+                b = prompt ('Во сколько обойдется?','');
             
-        appData.monthIncome = save/100/12*percent;
-        alert ('Доход v mesyac с вашего депозита: ' + appData.monthIncome);
+            if (typeof(a) === 'string' &&  typeof(a) != null && a !='' && typeof(b) != null && b !='' && a.length <20) {
+                console.log ('chooseExpenses - done');
+                appData.chooseExpenses[a] = b;
+            } else { 
+                console.log('cjooseExp - something wrong');
+                i--;
+            }
+        }
+    },
+    dectectDayBudget: function () {
+        appData.moneyPerDay = (appData.budget / 30).toFixed(3);
+        alert('Vash Budjet na odin den: ' + appData.moneyPerDay);
+    },
+    detectLeve: function () {
+        if(appData.moneyPerDay < 100){
+            alert('Маловато бабок у тебя сынок... надо чето думать');
+        } else if(appData.moneyPerDay  > 2000){
+            alert('Да ты жирный кот... где столько поднимаешь?');
+        } else if(appData.moneyPerDay >=100 && appData.moneyPerDay<= 2000){
+            alert('Ну не бедствуешь и ладно... как сам то братишка?');
+        } else {
+            alert('Гдето косяк братишка');
+        }
+    },
+    checkSavings: function (){
+        if (appData.savings == true) {
+            let save = +prompt('Какова сумма накоплений???'),
+                percent = +prompt('Под какой процент???');
+                
+            appData.monthIncome = save/100/12*percent;
+            alert ('Доход v mesyac с вашего депозита: ' + appData.monthIncome);
+        }
+    },
+    chooseOptExpenses: function() {
+        for (let i =0; i<=2; i++) {
+            let questionOptExp = prompt('Статья необязательных расходов?');
+            appData.optionalExpenses[i] = questionOptExp;
+        }
+    },
+    chooseIncome: function () {
+        let items = prompt('Что принесет доп доход? (Перечислите через запятую','');
+        if (typeof(items) !='string' || items =='' || (items) ==null) {
+            console.log('chooseIncome - введено некорректное значение или оно невведено вовсе');
+        } else {
+            appData.income = items.split(', ');
+            appData.income.push(prompt('Mojet chto-to eshe?',""));
+            appData.income.sort();
+        }
+        appData.income.forEach (function (itemmasive, i) {
+            alert('Sposobi dop zarabotka: ' + (i+1) + '-' + itemmasive);
+        });
     }
-
+};
+for (let key in appData) {
+    console.log('Nasha programma vkluchaet v sebya dannie: ' + key +' - ' + appData[key]);
 }
-
-checkSavings ();
-
-
-function chooseOptExpenses () {
-
-    for (let i =0; i<=2; i++) {
-
-        let questionOptExp = prompt('Статья необязательных расходов?');
-        appData.optionalExpenses[i] = questionOptExp;
-
-    }
-
-}
-chooseOptExpenses ();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
